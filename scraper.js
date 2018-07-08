9354bfaa98a5d5446657364ea30690ea8ab117ba
Jennifer Nordell <jennifer.nordell@outlook.com>
2018-07-08 21:20:30 +0200

    scraping of price working

--- scraper.js
+++ scraper.js
@@ -1,14 +1,37 @@
 
-
-const scrapeIt = require('scrape-it');
+const fs = require("fs");
+const scrapeIt = require("scrape-it");
+const siteRoot = `http://shirts4mike.com`;
 const mainPage = `http://shirts4mike.com/shirts.php`;
-
+const dataDirectory = "./data";
 
 
 scrapeIt(mainPage, {
-    title: "title"
+    title: "title",
+    shirts: {
+        listItem: ".products li",
+        data: {
+            url: {
+                selector: "a",
+                attr: "href"
+            }
+        }
+    }
     }
 ).then(({ data, response }) => {
-    console.log(`Status Code: ${response.statusCode}`);
-    console.log(data);
+    if (!fs.existsSync(dataDirectory)){
+        fs.mkdirSync(dataDirectory);
+    }
+ 
+    for(let shirt of data.shirts) {
+        scrapePage(shirt);
+    }
 })
+
+function scrapePage(pageURL) {
+    scrapeIt(`${siteRoot}/${pageURL.url}`, {
+       price: "h1 .price"
+    }).then(({ data, response }) => {
+        console.log(data.price);
+    })
+}
