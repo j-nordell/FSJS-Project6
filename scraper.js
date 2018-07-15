e1b054d642e86a60696a7279b82ff531156c4ab8
Jennifer Nordell <jennifer.nordell@outlook.com>
2018-07-15 23:45:00 +0200

    parses json and writes to csv file

--- scraper.js
+++ scraper.js
@@ -1,12 +1,15 @@
 
 const fs = require("fs");
+const Json2csvParser = require('json2csv').Parser;
 const scrapeIt = require("scrape-it");
 const siteRoot = `http://shirts4mike.com`;
 const mainPage = `http://shirts4mike.com/shirts.php`;
 const dataDirectory = "./data";
 const timeNow = new Date();
 const formattedTime = `${timeNow.getHours()}:${timeNow.getMinutes()}:${timeNow.getSeconds()}`;
-const formatedDay = `${timeNow.getFullYear()}-${timeNow.getMonth()}-${timeNow.getSeconds()}`;
+const formatedDay = `${timeNow.getFullYear()}-${timeNow.getMonth() + 1}-${timeNow.getDate()}`;
+const fields = ["Title", "Price", "ImageURL", "URL", "Time"];
+let shirtsResults = [];
 
 scrapeIt(mainPage, {
     shirts: {
@@ -18,19 +21,31 @@ scrapeIt(mainPage, {
             }
         }
     }
-    }
-).then(({ data, response }) => {
+}).then(({ data, response }) => {
     if (!fs.existsSync(dataDirectory)){
         fs.mkdirSync(dataDirectory);
     }
- 
+
     for(let shirt of data.shirts) {
         scrapePage(shirt);
     }
-})
+
+    setTimeout(() => {
+        try {
+            const parser = new Json2csvParser({ fields });
+            const csv = parser.parse(shirtsResults);
+            fs.writeFile(`${dataDirectory}/${formatedDay}.csv`, csv, (err) => {
+                if(err) {
+                    console.log(`Could not write to file: ${err.path}`);
+                }
+            });
+        } catch (err) {
+            console.log(err);
+        };
+    }, 5000);
+});
 
 function scrapePage(pageURL) {
-    //let wholeURL = `${siteRoot}/${pageURL.url}`;
     scrapeIt(`${siteRoot}/${pageURL.url}`, {
         title: "title",
         price: "h1 .price",
@@ -45,7 +60,9 @@ function scrapePage(pageURL) {
            ImageURL: data.imgURL,
            URL: `${siteRoot}/${pageURL.url}`,
            Time: formattedTime
-       }
-       console.log(scrapeResult);
-    })
+       };
+       shirtsResults.push(scrapeResult);
+    });
 }
+
+
